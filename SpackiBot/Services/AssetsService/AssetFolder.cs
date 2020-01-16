79da4e52275baff0afcb0113c6e7339c7c6aa759
Kitt3120 <kitt3120@aol.de>
2020-01-16 10:52:42 +0100

    WIP AssetsService
    Moved files
    Added AssetFolder

---
+++ SpackiBot/Services/AssetsService/AssetFolder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpackiBot.Services.AssetsService
+{
+    class AssetFolder
+    {
+        public List<AssetFolder> SubFolders { get; private set; }
+
+        public AssetFolder()
+        {
+
+        }
+    }
+}
---
+++ SpackiBot/Services/AssetsService/AssetsService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SpackiBot.Services
+{
+    public class AssetsService
+    {
+        public string ProjectRoot { get; private set; }
+
+        public AssetsService()
+        {
+            ProjectRoot = AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.IndexOf("bin"));
+        }
+
+        public bool ExistsDirectory(string path) => Directory.Exists(Get(path));
+        public bool ExistsFile(string path) => File.Exists(Get(path));
+        public string Get(string path) => Path.Combine(ProjectRoot, path);
+    }
+}
