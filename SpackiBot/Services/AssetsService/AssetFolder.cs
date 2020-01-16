a808ded32ab7ab97a3cfe373a55b0ef839065f58
Kitt3120 <Kitt3120@aol.de>
2020-01-16 16:25:43 +0100

    Finished AssetService
    AssetService now working as intended.
    Added wrapper functions to quickly gain access to AssetFolders. ( FindSub(); )
    Added wrapper functions to quickly read / write from / to files in AssetFolders . ( ReadFromAsync(); / WriteToAsync(); )

--- SpackiBot/Services/AssetsService/AssetFolder.cs
+++ SpackiBot/Services/AssetsService/AssetFolder.cs
@@ -1,16 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Linq;
 
 namespace SpackiBot.Services.AssetsService
 {
-    class AssetFolder
+    public class AssetFolder
     {
+        public AssetFolder ParentFolder { get; private set; }
+        public bool HasParentFolder { get => ParentFolder != null; }
         public List<AssetFolder> SubFolders { get; private set; }
+        public List<string> Files { get; private set; }
 
-        public AssetFolder()
+        public DirectoryInfo Information { get; private set; }
+
+        public AssetFolder(AssetFolder parentFolder, string directory)
         {
+            ParentFolder = parentFolder;
+            Information = new DirectoryInfo(parentFolder == null ? directory : Path.Combine(parentFolder.Information.FullName, directory));
 
+            RefreshSubs();
+            RefreshFiles();
         }
+
+        public void RefreshSubs()
+        {
+            SubFolders = new List<AssetFolder>();
+            foreach (var directory in Directory.EnumerateDirectories(Information.FullName))
+                SubFolders.Add(new AssetFolder(this, directory));
+        }
+
+        public void RefreshFiles()
+        {
+            Files = new List<string>();
+            foreach (var file in Directory.EnumerateFiles(Information.FullName))
+                Files.Add(Path.Combine(Information.FullName, file));
+        }
+
+        public List<AssetFolder> FilterSubs(string filter, StringComparison stringComparison) => SubFolders.Where(assetFolder => assetFolder.Information.Name.Contains(filter, stringComparison)).ToList();
+
+        public List<string> FilterFiles(string filter, StringComparison stringComparison) => Files.Where(file => file.Contains(filter, stringComparison)).ToList();
     }
 }
--- SpackiBot/Services/AssetsService/AssetService.cs
+++ SpackiBot/Services/AssetsService/AssetService.cs
@@ -1,21 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using SpackiBot.Logging;
+using SpackiBot.Services.AssetsService;
+using System;
 using System.IO;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SpackiBot.Services
 {
-    public class AssetService
+    public class AssetService : AssetFolder
     {
-        public string ProjectRoot { get; private set; }
+        private LoggingSection _loggingSection;
 
-        public AssetService()
+        public AssetService() : base(null, Path.Combine(AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.IndexOf("bin")), "Assets"))
         {
-            ProjectRoot = AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.IndexOf("bin"));
+            _loggingSection = new LoggingSection("AssetService");
+            _loggingSection.Verbose($"Loaded {SubFolders.Count} AssetFolders");
         }
 
-        public bool ExistsDirectory(string path) => Directory.Exists(Get(path));
-        public bool ExistsFile(string path) => File.Exists(Get(path));
-        public string Get(string path) => Path.Combine(ProjectRoot, path);
+        public AssetFolder FindSub(string path, StringComparison stringComparison) => FindSub(path.Split(Path.DirectorySeparatorChar), stringComparison);
+
+        public AssetFolder FindSub(string[] chain, StringComparison stringComparison)
+        {
+            if (chain.Length == 0)
+                return this;
+
+            AssetFolder assetFolder = FilterSubs(chain[0], stringComparison).FirstOrDefault();
+            if (assetFolder == null)
+                throw new DirectoryNotFoundException($"Directory {Path.Combine(Information.FullName, chain[0])} not found.");
+
+            for (int i = 1; i < chain.Length; i++)
+            {
+                AssetFolder next = assetFolder.FilterSubs(chain[i], stringComparison).FirstOrDefault();
+                if (next == null)
+                    throw new DirectoryNotFoundException($"Directory {Path.Combine(assetFolder.Information.FullName, chain[i])} not found.");
+                assetFolder = next;
+            }
+
+            return assetFolder;
+        }
+
+        public async Task WriteToAsync(AssetFolder assetFolder, string fileName, byte[] data, FileMode fileMode)
+        {
+            //await File.WriteAllBytesAsync(Path.Combine(assetFolder.FolderPath, fileName), data); Replaced by version below so file content can also be appended with FileMode.
+
+            using (FileStream fileStream = File.Open(Path.Combine(assetFolder.Information.FullName, fileName), fileMode))
+                await fileStream.WriteAsync(data);
+        }
+
+        public async Task<byte[]> ReadFromAsync(AssetFolder assetFolder, string fileName) => await File.ReadAllBytesAsync(Path.Combine(assetFolder.Information.FullName, fileName));
     }
 }
