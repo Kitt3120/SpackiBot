ba639e5c5ef1c13fc9393697a1a9ade18178f7db
Kitt3120 <Kitt3120@aol.de>
2020-01-16 01:49:05 +0100

    WIP Quote Module
    Quotes working now but still have to be sent in the right channel

---
+++ SpackiBot/Modules/Fun/Quote/QuoteModule.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpackiBot.Modules.Fun.Quote
+{
+    [Name("Quote")]
+    public class QuoteModule : ModuleBase<SocketCommandContext>
+    {
+        private static Random _colorRandom = new Random();
+
+        [Command("Quote")]
+        [Summary("Erstellt ein Zitat und postet es im Quote-Channel")]
+        [Alias(new string[] { "q" })]
+        public async Task QuoteAsync([Summary("Autor des Zitats")] SocketUser user = null, [Remainder] [Summary("Text des Zitats")] string text = null)
+        {
+            if (user == null || text == null)
+            {
+                await ReplyAsync("Du musst sowohl User als auch Text angeben!");
+                return;
+            }
+
+            var builder = new EmbedBuilder()
+                .WithAuthor(user)
+                .WithTitle(text)
+                .WithColor(new Color(_colorRandom.Next(255), _colorRandom.Next(255), _colorRandom.Next(255)))
+                .WithFooter(new EmbedFooterBuilder().WithText($"Zitat erstellt von {Context.User.Username}#{Context.User.Discriminator}"))
+                .WithTimestamp(DateTime.Now);
+
+            await ReplyAsync("", false, builder.Build());
+        }
+    }
+}
